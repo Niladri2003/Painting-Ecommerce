d506ef38ff7434ad2eab4528f680f69848049f85
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-08-08 22:36:35 +0530

    Create cartSlice and initialize reducer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'; 
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.jsx';
 import './index.css';
 
+import {configureStore} from '@reduxjs/toolkit';
+import rootReducer from './reducer';
+const store = configureStore({
+  reducer: rootReducer,
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider> 
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ChakraProvider>
+  <Provider store={store}>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
 );
