5a514b4e5b1503d830428df71cc868fc19cd3985
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-08-09 19:55:48 +0530

    Add auth and profile reducers to root reducer

--- client/src/reducer/index.jsx
+++ client/src/reducer/index.jsx
@@ -1,8 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import cartReducer from '../slices/cartSlice';
-
+import authReducer from '../slices/authSlice';
+import profileReducer from '../slices/profileSlice';
 
 const rootReducer = combineReducers({
     cart: cartReducer,
+    auth: authReducer,
+    profile: profileReducer,
+
 });
 export default rootReducer;
