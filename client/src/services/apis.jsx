c0419256b07bdbe5b82f44fad3fb92ba0fb28afd
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-08-09 18:13:14 +0530

    Update .gitignore and add apiConnector and apis files

---
+++ client/src/services/apis.jsx
@@ -0,0 +1,10 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export const endpoints = {
+    SIGNUP_API: BASE_URL + "/auth/signup",
+    LOGIN_API: BASE_URL + "/auth/login",
+} 
+
+export const cartEndpoints = {
+    
+}
