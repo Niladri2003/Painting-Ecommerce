0ac9338319629984d395c3b775348c55e0ed45a3
Abhishek Santra <abhisheksantra28@gmail.com>
2024-08-09 17:52:38 +0530

    Commented the sample data function

--- server/platform/migration/runmigration.go
+++ server/platform/migration/runmigration.go
@@ -1,15 +1,14 @@
 package migration
 
 import (
-	"database/sql"
+	
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/niladri2003/PaintingEcommerce/pkg/utils"
 	"log"
-	"os"
-	"path/filepath"
+	
 )
 
 func RunMigration() {
@@ -33,82 +32,82 @@ func RunMigration() {
 
 	log.Println("Database migrated successfully")
 	// Load sample data
-	loadSampleData(postgresConnURL)
+	// loadSampleData(postgresConnURL)
 }
 
-func tableExists(db *sql.DB, tableName string) (bool, error) {
-	var exists bool
-	query := `SELECT EXISTS (
-		SELECT 1
-		FROM information_schema.tables
-		WHERE table_schema = 'public'
-		AND table_name = $1
-	);`
-	err := db.QueryRow(query, tableName).Scan(&exists)
-	return exists, err
-}
+// func tableExists(db *sql.DB, tableName string) (bool, error) {
+// 	var exists bool
+// 	query := `SELECT EXISTS (
+// 		SELECT 1
+// 		FROM information_schema.tables
+// 		WHERE table_schema = 'public'
+// 		AND table_name = $1
+// 	);`
+// 	err := db.QueryRow(query, tableName).Scan(&exists)
+// 	return exists, err
+// }
 
-func loadSampleData(connURL string) {
-	db, err := sql.Open("postgres", connURL)
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	defer db.Close()
+// func loadSampleData(connURL string) {
+// 	db, err := sql.Open("postgres", connURL)
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect to database: %v", err)
+// 	}
+// 	defer db.Close()
 
-	// Tables to truncate
-	tables := []string{
-		"users",
-		"categories",
-		"products",
-		"product_images",
-		"orders",
-		"order_items",
-		"contacts",
-		"addresses",
-		"carts",
-		"cart_items",
-	}
+// 	// Tables to truncate
+// 	tables := []string{
+// 		"users",
+// 		"categories",
+// 		"products",
+// 		"product_images",
+// 		"orders",
+// 		"order_items",
+// 		"contacts",
+// 		"addresses",
+// 		"carts",
+// 		"cart_items",
+// 	}
 
-	// Truncate tables if they exist
-	for _, table := range tables {
-		exists, err := tableExists(db, table)
-		if err != nil {
-			log.Fatalf("Error checking if table exists: %v", err)
-		}
-		if exists {
-			_, err = db.Exec(fmt.Sprintf("TRUNCATE TABLE %s CASCADE;", table))
-			if err != nil {
-				log.Fatalf("Failed to truncate table %s: %v", table, err)
-			}
-			log.Printf("Table %s truncated successfully", table)
-		} else {
-			log.Printf("Table %s does not exist, skipping truncate", table)
-		}
-	}
+// 	// Truncate tables if they exist
+// 	for _, table := range tables {
+// 		exists, err := tableExists(db, table)
+// 		if err != nil {
+// 			log.Fatalf("Error checking if table exists: %v", err)
+// 		}
+// 		if exists {
+// 			_, err = db.Exec(fmt.Sprintf("TRUNCATE TABLE %s CASCADE;", table))
+// 			if err != nil {
+// 				log.Fatalf("Failed to truncate table %s: %v", table, err)
+// 			}
+// 			log.Printf("Table %s truncated successfully", table)
+// 		} else {
+// 			log.Printf("Table %s does not exist, skipping truncate", table)
+// 		}
+// 	}
 
-	// Determine the file path based on the environment variable
-	filePath := os.Getenv("SAMPLE_DATA_PATH")
-	if filePath == "" {
-		// Default path for local development
-		filePath = "platform/migration/sample_data.sql"
-	}
+// 	// Determine the file path based on the environment variable
+// 	filePath := os.Getenv("SAMPLE_DATA_PATH")
+// 	if filePath == "" {
+// 		// Default path for local development
+// 		filePath = "platform/migration/sample_data.sql"
+// 	}
 
-	// Get the absolute path
-	absPath, err := filepath.Abs(filePath)
-	if err != nil {
-		log.Fatalf("Failed to get absolute path: %v", err)
-	}
-	fmt.Printf("Absolute path to sample data file: %s\n", absPath)
+// 	// Get the absolute path
+// 	absPath, err := filepath.Abs(filePath)
+// 	if err != nil {
+// 		log.Fatalf("Failed to get absolute path: %v", err)
+// 	}
+// 	fmt.Printf("Absolute path to sample data file: %s\n", absPath)
 
-	data, err := os.ReadFile(absPath)
-	if err != nil {
-		log.Fatalf("Failed to read sample data file: %v", err)
-	}
+// 	data, err := os.ReadFile(absPath)
+// 	if err != nil {
+// 		log.Fatalf("Failed to read sample data file: %v", err)
+// 	}
 
-	_, err = db.Exec(string(data))
-	if err != nil {
-		log.Fatalf("Failed to execute sample data script: %v", err)
-	}
+// 	_, err = db.Exec(string(data))
+// 	if err != nil {
+// 		log.Fatalf("Failed to execute sample data script: %v", err)
+// 	}
 
-	log.Println("Sample data loaded successfully")
-}
+// 	log.Println("Sample data loaded successfully")
+// }
