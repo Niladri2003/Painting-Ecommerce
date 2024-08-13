a92a8544d192ece68eb3a4291ad96dc89678ebfc
Abhishek Santra <abhisheksantra28@gmail.com>
2024-08-13 18:06:43 +0530

    Update cart endpoints and add delete cart functionality

--- client/src/services/apis.jsx
+++ client/src/services/apis.jsx
@@ -9,6 +9,7 @@ export const cartEndpoints = {
     CREATE_CART: BASE_URL + "/create-cart",
     ADD_ITEM: BASE_URL + "/add-item",
     GET_CART: BASE_URL + "/get-cart",
-    REMOVE_ITEM: BASE_URL + "/remove-item/1144ac99-9024-449e-a293-6c0c2f4f74b3",
-    UPDATE_ITEM: BASE_URL + "/update-item"
+    REMOVE_ITEM: BASE_URL + "/remove-item/1144ac99-9024-449e-a293-6c0c2f4f74b3", //params -> itemId
+    UPDATE_ITEM: BASE_URL + "/update-item",
+    DELETE_CART: BASE_URL + "/delete-cart", // It will delete all the items in the cart
 }
