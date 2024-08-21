f2f67e84bc997acb5da7f974cb9de535b79ae05f
Abhishek Santra <abhisheksantra28@gmail.com>
2024-08-21 20:41:50 +0530

    Revert "Modified some function"
    This reverts commit f95f681321f19eda60cf83b3b7127310970315c3.
    Revert the last changes

--- client/src/reducer/index.jsx
+++ client/src/reducer/index.jsx
@@ -1,11 +1,12 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import cartReducer from "../slices/cartSlice";
-import authReducer from "../slices/authSlice";
-import profileReducer from "../slices/profileSlice";
+import { combineReducers } from '@reduxjs/toolkit';
+import cartReducer from '../slices/cartSlice';
+import authReducer from '../slices/authSlice';
+import profileReducer from '../slices/profileSlice';
 
 const rootReducer = combineReducers({
-  [cartReducer.name]: cartReducer,
-  [authReducer.name]: authReducer,
-  [profileReducer.name]: profileReducer,
+    cart: cartReducer,
+    auth: authReducer,
+    profile: profileReducer,
+
 });
 export default rootReducer;
