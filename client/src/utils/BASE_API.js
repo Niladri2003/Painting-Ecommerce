1ef28ad891fc3930ecd3b6746f3b1fe7b5a9c4af
Niladri2003 <niladri.adak.21@aot.edu.in>
2024-08-22 11:34:21 +0530

    ProductCheckout modified

--- client/src/utils/BASE_API.js
+++ client/src/utils/BASE_API.js
@@ -1,4 +1,4 @@
 
-//export const BASEAPI = "https://ecommerce.niladriadak.tech/api/v1";
-export const BASEAPI = "http://127.0.0.1:5000/api/v1";
+export const BASEAPI = "https://ecommerce.niladriadak.tech/api/v1";
+//export const BASEAPI = "http://127.0.0.1:5000/api/v1";
 
