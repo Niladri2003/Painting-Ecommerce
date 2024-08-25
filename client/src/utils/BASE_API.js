9776f8f55875babcf2bd9ff0ca381772be43ba7a
Abhishek Santra <108479463+Abhisheksantra28@users.noreply.github.com>
2024-08-25 16:32:27 +0530

    Update BASE_API.js

--- client/src/utils/BASE_API.js
+++ client/src/utils/BASE_API.js
@@ -1,5 +1,5 @@
 
-// export const BASEAPI = "https://ecommerce.niladriadak.tech/api/v1";
-export const BASEAPI = "https://testserver.niladriadak.tech/api/v1";
+export const BASEAPI = "https://ecommerce.niladriadak.tech/api/v1";
+//export const BASEAPI = "https://testserver.niladriadak.tech/api/v1";
 //export const BASEAPI = "http://127.0.0.1:5000/api/v1";
 
